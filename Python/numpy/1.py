22483deaf586c11bb0fe0c774d88fee5f6adac36
Ali Abbas <aa0504503@gmail.com>
2026-01-17 08:43:14 -0500

    Dot product

--- Python/numpy/1.py
+++ Python/numpy/1.py
@@ -16,5 +16,27 @@ a=array([[[ 1,  2,  3],
         [13, 14, 15],
         [16, 17, 18]]])
 
-a
+higher_var_array = np.array([1,100,200,300,4000,5000])
+low_var_array = np.array([2,4,6,8,10])
+np.var(higher_var_array) , np.var(low_var_array)
 
+np.std(higher_var_array) , np.std(low_var_array)
+np.mean(higher_var_array) , np.mean(low_var_array)
+
+%matplotlib inline 
+import matplotlib.pyplot as plt
+plt.hist(higher_var_array)
+plt.show()
+
+plt.hist(low_var_array)
+plt.show()
+
+
+np.random.seed(0)
+mat1 = np.random.randint(10, size=(5,3))
+mat2 = np.random.randint(10, size=(5,3))
+mat1, mat2
+
+mat1.T
+mat3 = np.dot(mat1,mat2.T)
+mat3,mat3.shape
